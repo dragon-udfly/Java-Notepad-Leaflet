7092160f3de6d2380f8a271298322a36c73f3431
Isuru Udakara <pereraisuruudakara@gmail.com>
2025-08-10 12:54:24 +0530

    Created new instance for JFrame and created a basic JFrame window

--- GUI.java
+++ GUI.java
@@ -1,9 +1,23 @@
+import javax.swing.JFrame;
+
 public class GUI{
+
+    JFrame window;
     public static void main(String args[]){
         new GUI();
     }
 
     public GUI(){
-        
+        // call methods below
+        createWindow();
+
+        // call method above
+        window.setVisible(true);
+    }
+
+    public void createWindow(){
+        window= new JFrame("Leaflet");
+        window.setSize(900, 700);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
