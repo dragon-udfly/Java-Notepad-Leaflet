ea6384dfef83797e9d7be8721bcac5f3537c845d
Isuru Udakara <pereraisuruudakara@gmail.com>
2025-08-15 11:56:01 +0530

    Chagned background and foreground color of text area nad window

--- Function_Color.java
+++ Function_Color.java
@@ -1,3 +1,5 @@
+import java.awt.Color;
+
 public class Function_Color {
     GUI gui;
 
@@ -6,6 +8,22 @@ public class Function_Color {
     }
 
     public void changeColor(String color){
-
+        switch(color){
+            case "Red":
+            gui.window.getContentPane().setBackground(Color.red);
+            gui.textArea.setBackground(Color.red);
+            gui.textArea.setForeground(Color.white);
+            break;
+            case "White":
+            gui.window.getContentPane().setBackground(Color.white);
+            gui.textArea.setBackground(Color.white);
+            gui.textArea.setForeground(Color.black);
+            break;
+            case "Blue":
+            gui.window.getContentPane().setBackground(Color.blue);
+            gui.textArea.setBackground(Color.blue);
+            gui.textArea.setForeground(Color.white);
+            break;
+        }
     }
 }
