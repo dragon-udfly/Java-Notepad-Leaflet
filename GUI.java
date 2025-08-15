ab940eaa688854b4e8c6d78b582a3960ff8c8115
Isuru Udakara <pereraisuruudakara@gmail.com>
2025-08-15 12:12:00 +0530

    Declared two JMenuItem instances for undo and redo

--- GUI.java
+++ GUI.java
@@ -20,6 +20,7 @@ public class GUI implements ActionListener{
     JMenuItem itemFontSize8, itemFontSize12, itemFontSize16, itemFontSize20, itemFontSize24, itemFontSize28;
     JMenu menuFont, menuFontSize;
     JMenuItem iColor1, iColor2, iColor3;
+    JMenuItem iUndo, iRedo;
 
     boolean wordWrapOn= false;
 
